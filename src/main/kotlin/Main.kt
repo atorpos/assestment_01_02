453930a8def02d894eefbfa670ca8dc67e4a95b5
Oskar Wong <oskar.wong@me.com>
2025-01-09 11:26:46 +0800

    trail

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,76 @@
 package org.fxexchange
 
 fun main() {
-    println("Hello World!")
+
+    val employees = listOf(
+        Employee(1, "John", "IT", 95000.0, listOf(Project("A", 50000.0, Status.ACTIVE), Project("C", 30000.0, Status.COMPLETE))),
+        Employee(2, "Mary", "HR", 85000.0, listOf(Project("B", 50000.0, Status.ACTIVE))),
+        Employee(3, "Peter", "IT", 75000.0, listOf(Project("C", 20000.0, Status.ON_HOLD))),
+    )
+
+    val average_sal = mutableMapOf<String, MutableList<Double>>()
+    val activeProjectBudgets = mutableMapOf<String, Double>()
+
+    for (employee in employees) {
+        val active_projects = employee.projects.filter { it.status == Status.ACTIVE }
+        if(!average_sal.containsKey(employee.department)) {
+            average_sal[employee.department] = mutableListOf()
+        }
+        average_sal[employee.department]?.add(employee.salary)
+
+        for (project in active_projects) {
+            activeProjectBudgets[employee.department] = activeProjectBudgets.getOrDefault(employee.department, 0.0) + project.budget
+        }
+    }
+
+    var highavg_sal = mutableListOf<String>()
+    for ((department, sals) in average_sal) {
+        val average_sal = sals.average() / sals.size
+        if (average_sal >= 80000) {
+            highavg_sal.add(department)
+        }
+    }
+
+    println("1: Departments with high average salary over 80000: $highavg_sal")
+
+
+    val active_project_sal = mutableListOf<Project>()
+    for (employee in employees) {
+        for (project in employee.projects) {
+            if (project.status == Status.ACTIVE) {
+                active_project_sal.add(project)
+            }
+        }
+    }
+
+    val departmentWithHighBudget = activeProjectBudgets.filter { it.value > 100000 }
+
+    println("2: Active projects: $active_project_sal")
+
+    println("3: Total budget for active projects:")
+    for ((department, budget) in activeProjectBudgets) {
+        println("$department: $budget")
+    }
+
+    println("4: Department with total active project budget exceeding 100000:")
+    for ((deparmnet, totalBudget) in departmentWithHighBudget) {
+        println("$deparmnet: $totalBudget")
+    }
+
 }
+
+data class Employee(
+    val id: Int,
+    val name: String,
+    val department: String,
+    val salary: Double,
+    val projects: List<Project>
+)
+
+data class Project(
+    val id: String,
+    val budget: Double,
+    val status: Status
+)
+
+enum class Status {ACTIVE, COMPLETE, ON_HOLD}
--- src/main/kotlin/task2.kt
+++ src/main/kotlin/task2.kt
@@ -1,2 +1,48 @@
-class task2 {
+package org.fxexchange
+
+import java.time.LocalDateTime
+
+fun main() {
+
+
 }
+
+
+fun analyzeTransactions(transactions: List<Transaction>): List<UserStats> {
+    val transactionByUser = transactions.groupBy { it.userId }
+
+    val userStats = transactionByUser.mapNotNull { (userId, transactions) ->
+        val completedTransactions = transactions.filter { it.status == TransactionStatus.COMPLETED }
+        val totalAmount = completedTransactions.sumOf { it.amount }
+        val avgAmount = totalAmount / completedTransactions.size
+        val topCategory = completedTransactions.groupingBy { it.category }
+            .eachCount()
+            .maxByOrNull { it.value } // Get the category with the highest count
+            ?.key ?: "N/A"
+        val recentTransactionsCount = completedTransactions.count { it.date.isAfter(LocalDateTime.now().minusDays(30)) }
+
+        UserStats(userId = userId, totalAmount = totalAmount, avgAmount = avgAmount, topCategory = topCategory, recentTransactionsCount = recentTransactionsCount)
+    }
+
+    return userStats.sortedByDescending { it.totalAmount }
+
+}
+
+data class Transaction(
+    val id: String,
+    val userId: Int,
+    val category: String,
+    val amount: Double,
+    val date: LocalDateTime,
+    val status: TransactionStatus
+)
+
+enum class TransactionStatus { COMPLETED, PENDING, FAILED }
+
+data class UserStats(
+    val userId: Int,
+    val totalAmount: Double,
+    val avgAmount: Double,
+    val topCategory: String,
+    val recentTransactionsCount: Int
+)
